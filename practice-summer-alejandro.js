6ec992cb95604b57329ed9620310b727161f0e39
summer-cook <summercook@bellsouth.net>
2020-11-03 15:30:35 -0800

    Answered string, boolean, and variable challenges

--- practice-summer-alejandro.js
+++ practice-summer-alejandro.js
@@ -1,8 +1,27 @@
 // Hi Alejandro!
 //Hey Summer
+
+//arithmetic questions
 console.log(34 + 71)
 console.log(123 - 67);
 console.log(56 * 23);
 console.log(45 / 5);
 console.log(Math.pow(5,7));
 console.log(33 % 6);
+//string challenges
+var name = "Summer"
+console.log(name.length)
+console.log(name.includes("e"))
+//boolean challenges
+console.log(34/3 > 67/2)
+console.log(5 == "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 && "Student".length === 7)
+//variable Challenges
+var favNum = 14
+console.log(14/2)
+var otherNum = 13
+console.log(favNum / otherNum)
+var string = "string"
+console.log(string.length);
+console.log(string.charAt(3))
