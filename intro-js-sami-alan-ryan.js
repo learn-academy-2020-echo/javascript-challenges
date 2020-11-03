82a24223604f5ac65d48d560d074ec0c9569eeb7
mabrouk10 <samimabrouk10@gmail.com>
2020-11-03 15:28:24 -0800

    conditionals

--- intro-js-sami-alan-ryan.js
+++ intro-js-sami-alan-ryan.js
@@ -3,17 +3,51 @@
 */
 //hello alan
 //hope this gets to you
-console.log(34+71);
-console.log(67-123);
-console.log(56*23);
-console.log(46/5);
-console.log(5^7);
-console.log(33%6);
 
-// swtich to ryan
-var catName = "Ryan"
-console.log(catName.length)
-console.log(catName === "e")
-console.log((34/3) > (67/2));
-console.log(5 == "5");
-console.log(5 === "5");
+
+// console.log(34+71);
+// console.log(67-123);
+// console.log(56*23);
+// console.log(46/5);
+// console.log(5^7);
+// console.log(33%6);
+
+// // swtich to ryan
+// var catName = "Ryan"
+// console.log(catName.length)
+// console.log(catName === "e")
+// console.log((34/3) > (67/2));
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+
+// var price = 200;
+
+// if (price < 100){
+//     console.log("in budget")
+// }else{
+//     console.log("too expensive")
+// }
+
+
+// var hunger = true;
+
+// if (hunger = true){
+//     console.log("eat food")
+// }else{
+//     console.log("keep coding")
+// }
+
+
+var trafficLight = "joiewajoi"
+
+if (trafficLight === "green"){
+    console.log("go")
+}else if (trafficLight === "yellow"){
+    console.log("slow down")
+}else if (trafficLight === "red"){
+    console.log("stop")
+}else{
+    console.log("you're probably not driving")
+}
+
