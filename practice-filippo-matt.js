089ef6c66f3328001d47cbb09f05531821d47f53
M-ram224 <mattramirez2011@gmil.com>
2020-11-03 15:29:27 -0800

    Did Numbers through Booleans

---
+++ practice-filippo-matt.js
@@ -0,0 +1,22 @@
+// Number Challenges
+// console.log(34+71);
+// console.log(123-67);
+// console.log(56*23);
+//console.log(45/5);
+//console.log(5**7);
+//console.log(33%6);
+
+// String Challenges
+// var name = "Matthew"
+// console.log(name.length);
+// console.log(name.includes("e"));
+
+// Boolean Challenges
+console.log(34/3 < 67/2);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(!3 === 3);
+
+var school = "LEARN"
+var student = "Student"
+console.log(school.length === 5 && student.length === 7);
