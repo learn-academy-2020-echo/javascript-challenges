1898f85848e4e774b5322f0928d3c3a01bfb9db5
mabrouk10 <samimabrouk10@gmail.com>
2020-11-04 14:16:30 -0800

    more loop practice

--- loops-sami-priscila.js
+++ loops-sami-priscila.js
@@ -12,13 +12,57 @@
 //     console.log(index * 3);   
 // }
 
-// for (let index = 0; index < 20; index++) {
+// var array = [4, 57, -9, 20, 67]
+
+// for (let index = 0; index < array.length; index++) {
 //     if(index % 2 == 0){
 //         console.log(index)
 //     } else {
 //         console.log("Odd")
 //     } 
 // }
+
+// var array = [3, 57, -9, 20, 67]
+// var largest = 0;
+
+//     for (i=0; i<array.length; i++) {
+//         largest = array[i] % 2
+//         console.log(largest);
+//     }
+
+
+// var array = [3, 57, -9, 20, 67]
+// var largest = 0;
+//    for (i=0; i<array.length; i++) {   
+//        if (array[i]>largest) {        
+//            largest = array[i];
+//         }
+//     }
+//  console.log(largest);
+
+
+
+// var array = [3, 57, -9, 20, 66]
+// var remainder = 0;
+
+//     for (i=0; i<array.length; i++) {
+//         remainder = array[i] % 2
+//         console.log(remainder);
+//     }
+
+
+var myString = "learn student"
+var count = 0;
+
+for (let index = 0; index < myString.length; index++) {
+    if(myString[index] == 'e'){
+        count++
+    }    
+}
+
+console.log(count);
+
+
 
 
 
