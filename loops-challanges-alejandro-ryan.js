b6d722d92047f88448a9ad42b5776c3e244ff054
lionsnarl <ryan.sarll@hotmail.com>
2020-11-04 11:43:52 -0800

    loops challanges

---
+++ loops-challanges-alejandro-ryan.js
@@ -0,0 +1,17 @@
+/* ready to
+rock :) */
+//
+// for (let i = 0; i <= 20; i++){
+//   console.log(i);
+// }
+// for (let i = 0; i <= 20; i++){
+//   console.log(i * 3);
+// }
+for (let i = 0; i <= 20; i++){
+    // console.log(i);
+
+  if((i % 2 !== 0))
+    console.log("odd");
+    else(console.log(i))
+  }
+// hoorrah!!!
