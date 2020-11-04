fe29f1cfaa460c4c7f68bd8dffe81b5cc7b1340b
filippo20u <filippo.20u@gmail.com>
2020-11-03 18:05:02 -0800

    end of Tuesday class

--- practice-filippo-matt.js
+++ practice-filippo-matt.js
@@ -12,11 +12,36 @@
 // console.log(name.includes("e"));
 
 // Boolean Challenges
-console.log(34/3 < 67/2);
-console.log(5 == "5");
-console.log(5 === "5");
-console.log(!3 === 3);
+// console.log(34/3 < 67/2);
+// console.log(5 == "5");
+// console.log(5 === "5");
+// console.log(!3 === 3);
 
-var school = "LEARN"
-var student = "Student"
-console.log(school.length === 5 && student.length === 7);
+// var school = "LEARN"
+// var student = "Student"
+// console.log(school.length === 5 && student.length === 7);
+
+
+//Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// var moneySpent = "40"
+
+// if(moneySpent <= 100) {
+//     console.log("you are in budget")
+// }
+// else {
+//     console.log("you spent too much")
+// }
+
+// Write an if/else statement that takes a variable of hungry and logs "eat food" 
+// if you are hungry and "keep coding" if you are not hungry.
+
+var hungry
+var hungry=false
+
+if(hungry=true) {
+    console.log("eat food")
+} else {
+    console.log("keep coding")
+}
+
