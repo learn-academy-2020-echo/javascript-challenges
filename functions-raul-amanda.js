4b33d59ca80a6ff26467c802b562c05f645f0ad2
REV004 <rsier004@gmail.com>
2020-11-04 15:35:57 -0800

    start function challenge

---
+++ functions-raul-amanda.js
@@ -0,0 +1,46 @@
+// DON'T FORGET TO PSEUDOCODE
+//
+// declare a function named marco that returns "polo".
+// const marco = () => {
+// // retun "polo"
+//   return "polo"
+// }
+// // call function with console.console.log()
+// console.log(marco())
+
+// ``
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// declare a  function that takes in name as an argument
+// const greeting = (name) => {
+// //returns message "Welcome, <person's name here>!"
+//  return `Welcome, ${name}!`
+// }
+// // call function with console.log()
+// console.log(greeting("Amanda"))
+//
+
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// declare the function that takes a number as an argument
+const oddOrEven = (num) => {
+// conditional that determines if its odd or even
+// return if number is oddOrEven
+if (num % 2 === 0){
+  return "even"
+} else {
+  return "odd"
+}
+
+}
+// call the function
+console.log(oddOrEven(35))
+console.log(oddOrEven(44))
+
+//
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+//
+// Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+//
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+//
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
--- loops-raul-amanda.js
+++ loops-raul-amanda.js
@@ -1,7 +1,7 @@
 // Logging values with for loops
 // Write a for loop that logs each number from 1 - 20.
 
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 // for(let i = 1; i <= 20; i++) {
 //     console.log(i)
@@ -14,26 +14,40 @@ let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 // }
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD" Expected output --> ODD, 2, ODD, 4, ODD, 6 ...etc
 
-for(let i = 0; i < numbers.length; i++) {
-    if(numbers[i] % 2 === 0) {
-        console.log(numbers[i])
-    } else {
-        console.log('ODD')
-    }
-}
+// for(let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 === 0) {
+//         console.log(numbers[i])
+//     } else {
+//         console.log('ODD')
+//     }
+// }
 
 
 // Looping over an array. Consider this variable:
 var nums = [3, 57, -9, 20, 67]
 
 // Create a loop that will log the highest number from the array. Expected output --> 67
-
-for(let i = 0; i <= nums.length; i++) {
-    
-}
-
+// let max = nums[0]
+// for(let i = 0; i <= nums.length; i++) {
+//   if (max < i ){
+//     max = nums[i]
+//   }
+// }
+//
+// console.log(max)
 // Create a loop that will log the lowest number from the array Expected output --> -9
+// let min = nums[0]
+// for(let i = 0; i <= nums.length; i++) {
+//   if (min > i){
+//     min = nums[i]
+//   }
+// }
+// console.log(min)
 // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+
+for (let i = 0; i <= nums.length; i++){
+  console.log(nums[i] % 2)
+}
 // Looping over a string. Consider this variable:
 // var myString = "learn student"
 // Write the code that will log the number of times the letter "e" occurs in the string. Expected output --> 2
