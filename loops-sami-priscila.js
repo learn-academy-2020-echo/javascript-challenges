9bfe0b9e85ae6b37256e318ee92ac4c884242e28
mabrouk10 <samimabrouk10@gmail.com>
2020-11-04 11:35:44 -0800

    loops practice

---
+++ loops-sami-priscila.js
@@ -0,0 +1,27 @@
+//Loops practice
+
+
+
+
+// for (let index = 0; index < 20; index++) {
+//     console.log(index);   
+// }
+
+
+// for (let index = 0; index < 20; index++) {
+//     console.log(index * 3);   
+// }
+
+// for (let index = 0; index < 20; index++) {
+//     if(index % 2 == 0){
+//         console.log(index)
+//     } else {
+//         console.log("Odd")
+//     } 
+// }
+
+
+
+
+
+
