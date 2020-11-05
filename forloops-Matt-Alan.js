0fae21737896586f4a5913f4bae9ed44897b5268
M-ram224 <mattramirez2011@gmil.com>
2020-11-04 16:48:22 -0800

    Still working on functions

---
+++ forloops-Matt-Alan.js
@@ -0,0 +1,61 @@
+// for(let i = 1; i < 20; i++){
+//   console.log(i * 3);
+// }
+//
+// var oddNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// for(let i = 0; i < oddNumbers.length; i++)
+//   {if (oddNumbers[i] % 2 !== 0){
+//     console.log("ODD");
+//   }else {
+//     console.log(oddNumbers[i]);
+//
+//   }
+// }
+// var nums = [3, 57, -9, 20, 67]
+//   for (let i = 0; i < nums.lastIndexOf(67); i++)
+// {console.log(nums[i]);}
+
+
+
+// Starting Functions
+// Write a function named marco that returns "polo".
+// const marco = () => {
+//     return "polo"
+//
+// }
+//     console.log(marco());
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+// const person = (name) => {
+//     return `welcome, ${name}!`
+//   }
+//     console.log(person('matt'));
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+// const oddOrEven =(number) => {
+//   if (number % 2 === 1){
+//     return "odd"
+//   } else if (number % 2 === 0) {
+//     return "even"
+//   }
+// }
+// console.log(oddOrEven(4));
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+const triple = (number) => {
+    if (true) {
+
+    }
+
+}
+
+
+
+
+
+
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+//
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+//
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
