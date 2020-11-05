e7c94c33da5311ebf8f7808d22984a71fa90d17e
REV004 <rsier004@gmail.com>
2020-11-04 16:48:11 -0800

    Finish

--- functions-raul-amanda.js
+++ functions-raul-amanda.js
@@ -53,15 +53,15 @@
 //
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
 // Declare the function named multiply that takes two number arguments
-const multiply = (num1, num2) => {
-// Return number arguments multiplyed by each other
-return num1 * num2
-}
-
-// Call the function
-console.log(multiply(2, 3))
-console.log(multiply(5, 10))
-console.log(multiply(4, 2))
+// const multiply = (num1, num2) => {
+// // Return number arguments multiplyed by each other
+// return num1 * num2
+// }
+//
+// // Call the function
+// console.log(multiply(2, 3))
+// console.log(multiply(5, 10))
+// console.log(multiply(4, 2))
 
 
 
@@ -70,8 +70,41 @@ console.log(multiply(4, 2))
 //
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
+// Declare the function divisibleBy that takes two numbers as arguments
+const divisibleBy = (num1, num2) => {
+// conditional to see if the first number is evenly divisble by the second
+if ( num1 % num2 === 0){
+// return a statement that says num1 is evenly divisible by num2
+return `${num1} is evenly divisble by ${num2}`
+}
+
+}
+// Call the function
+console.log(divisibleBy(10, 5))
+
 
 
 
 //
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Declare function namedmassignGrade that takes a number as an arguments
+const assignGrade = (score) => {
+// Conditional to determine letter grade
+if (score >= 90){
+  return "A"
+} else if (score >= 80){
+  return "B"
+} else if (score >= 70){
+  return "C"
+} else if (score >= 60){
+  return "D"
+} else if (score >= 59){
+  return "F"
+} else
+
+  }
+// return letter grade for the score
+}
+
+// call the function
