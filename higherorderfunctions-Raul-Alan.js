cc737bf8e90cca7c35fa8a631c4d444c085e2b35
AlanHausinger <alanh621@gmail.com>
2020-11-05 15:38:10 -0800

    finishchallanges

---
+++ higherorderfunctions-Raul-Alan.js
@@ -0,0 +1,10 @@
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const removeNums = (nums1) => {
+  return nums1.filter(comboArr)
+}
+
+
+
+var comboArr = comboArr.join("nice")
+console.log(removeNums(comboArr));
