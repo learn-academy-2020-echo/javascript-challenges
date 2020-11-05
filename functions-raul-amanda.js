24364f8c151246006b7f48d6506146ea2090cdc0
AKFosterDev <akfoster2015@gmail.com>
2020-11-04 16:05:47 -0800

    Continue function challenges

--- functions-raul-amanda.js
+++ functions-raul-amanda.js
@@ -22,25 +22,56 @@
 
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 // declare the function that takes a number as an argument
-const oddOrEven = (num) => {
-// conditional that determines if its odd or even
-// return if number is oddOrEven
-if (num % 2 === 0){
-  return "even"
-} else {
-  return "odd"
-}
+// const oddOrEven = (num) => {
+// // conditional that determines if its odd or even
+// // return if number is oddOrEven
+// if (num % 2 === 0){
+//   return "even"
+// } else {
+//   return "odd"
+// }
 
-}
-// call the function
-console.log(oddOrEven(35))
-console.log(oddOrEven(44))
+// }
+// // call the function
+// console.log(oddOrEven(35))
+// console.log(oddOrEven(44))
 
-//
-// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// //
+// // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+// // Declare the function named triple with a number as an argument
+// const triple = (num) => {
+//   // Return the argument multiplied by 3
+//   return num * 3
+// }
+
+// // Call the function
+// console.log(triple(4))
+// console.log(triple(5))
+// console.log(triple(6))
+
+
 //
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+// Declare the function named multiply that takes two number arguments
+const multiply = (num1, num2) => {
+// Return number arguments multiplyed by each other
+return num1 * num2
+}
+
+// Call the function
+console.log(multiply(2, 3))
+console.log(multiply(5, 10))
+console.log(multiply(4, 2))
+
+
+
+
+
 //
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+
+
+
 //
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
