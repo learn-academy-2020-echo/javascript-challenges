28e21d8ae61846cb53679ae429f58c60d45847f5
summer-cook <summercook@bellsouth.net>
2020-11-05 14:16:03 -0800

    initial commit

---
+++ higher-order-functions-summer-priscila.js
@@ -0,0 +1,48 @@
+// var arr1 = [3,9,15,4,10].map(value => {
+//     return value * 10
+//   })
+// console.log(arr1);
+
+
+// var arr2 = [2, 7, 3, 5, 8, 10, 13]
+// //declare a function that takes an array as an argument
+// const returnOdd = (array) => {
+//   return array.filter(value => {
+//     return value % 2 !== 0
+//   })
+// }
+// //return a higher-order function that takes in the value
+// //return only that values that are odd
+// //log the function and pass in the array
+// console.log(returnOdd(arr2))
+
+
+//this was a messup
+// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+// //declare a function that takes an array as an argument
+// //return a higher-order function that takes in the values
+// //return a string with only the letters
+// const returnString = (array) => {
+//   return array.filter(value => {
+//     return typeOf(value) === string
+//   })
+// }
+// console.log(returnString(comboArr.join("")))
+
+
+
+
+
+
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+//declare a function that takes an array as an argument
+//return a higher-order function that takes in the values
+//return a string with only the letters
+const returnString = (array) => {
+  return array.filter(value => {
+    if(typeof value === "string"){
+      return value
+    }
+  }).join("")
+}
+console.log(returnString(comboArr))
