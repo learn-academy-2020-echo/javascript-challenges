31b405e749e8cfb5f8c1c9c5208bd09c37d63ff2
filippo20u <filippo.20u@gmail.com>
2020-11-04 16:09:53 -0800

    functions

---
+++ loops-summer-filippo-chris.js
@@ -0,0 +1,66 @@
+// for(let i = 1; i <= 20; i++){
+//   console.log(i)
+// }
+//
+//
+// for(let i = 1; i <=20; i++)
+//   if(i % 2 === 0){
+//   console.log(i)
+//   } else {
+//   console.log("ODD")
+//   }
+
+// var nums = [3, 57, -9, 20, 67]
+// var lowestNumber = nums[0]
+//this is what is happening in the loop
+//highestNumber = 3
+//highestNumber = 57
+//loop doesn't execute for -9
+//loop doesn't execute for 20
+//highestNumber = 67
+
+// for(let i = 0; i < nums.length; i++){
+//
+//   if(nums[i] < lowestNumber) {
+//     lowestNumber = nums[i]
+//   }
+// }
+// console.log(lowestNumber);
+
+// var nums = [3, 57, -9, 20, 67]
+//
+//   for(let i=0; i<nums.length; i++){
+//     console.log(nums[i] % 2)
+//   }
+
+
+
+// declare a function named Marco that when called will return Polo
+// const marco = () => {
+//   return "Polo"
+// }
+// console.log(marco());
+
+// Write a function named greeting that takes a name as an argument
+// and returns "Welcome, <person's name here>!"
+
+// 3 Write a function named oddOrEven that takes a number as an argument
+// and returns whether the number is odd or even.
+
+// declare a function calle oddOrEven that takes a variable as an argument
+// establish whether the variable is an odd or even number
+// if the variable is an odd number return "odd"
+// if the variable is an even number return "even"
+
+// let oddOrEven = 2
+
+const oddOrEven = () => {
+  if oddOrEven %2 ===0
+
+} {return "even"}
+  else {
+  return "odd"
+}
+}
+
+console.log(oddOrEven(2));
