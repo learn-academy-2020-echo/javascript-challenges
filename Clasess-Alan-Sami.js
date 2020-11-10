06a6d7c93d7ce71fb642fb87a5f846e4fc34ed1f
AlanHausinger <alanh621@gmail.com>
2020-11-09 16:44:12 -0800

    updates

--- Clasess-Alan-Sami.js
+++ Clasess-Alan-Sami.js
@@ -119,18 +119,21 @@
 // class Tesla inherits from class Car
 // Create an object called myTesla which is a instance of class Tesla
 // Story: As a programmer, I can give my Tesla a model on initialization.
-// The model can be inherited from the parent class Car by passing the model 
+// The model can be inherited from the parent class Car by passing the model
 //through the constructor() and super() on the child class
 
 
 class Car {
-    constructor(model, year, wheels){
+    constructor(model, year, wheels, lights = "off", signalProperty = "off", speed = 0){
         this.model = model
         this.year = year
         this.wheels = wheels
+        this.lights = lights
+        this.signalProperty = signalProperty
+        this.speed = speed
     }
     carProfile(){
-        return `This is a ${this.model} car it was made in ${this.year} and it has ${this.wheels} wheels`
+        return `This is a ${this.model} car it was made in ${this.year} and it has ${this.wheels} wheels and its lights are ${this.lights} and the signals are ${this.signalProperty} and it ${this.speed} 60 really quick`
     }
     carWheels(){
         return `This car has ${this.wheels}`
@@ -138,11 +141,13 @@ class Car {
 }
 
 class Tesla extends Car{
-    constructor(model, year, wheels){
+    constructor(model, year, wheels, lights){
         super()
         this.model = model
         this.year = year
         this.wheels = wheels
+        this.lights = lights
+        this.speed = this.speed + 10
     }
 
     fuel(){
@@ -156,11 +161,55 @@ var myCar = new Car('generic', 2010, 10)
 console.log(myCar.carProfile());
 
 
-var myTesla = new Tesla('model-x', 2020, 4)
+var myTesla = new Tesla('model-x', 2020, 4, ``, ``, 70)
 console.log(myTesla.carProfile())
 
 
+// /Story: As a programmer, I can make a Toyota car.
+// class Toyota inherits from class Car
+// create an object called myToyota which is a instance of class Toyota
+// Story: As a programmer, I can give my Toyota a model on initialization.
+// The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
+// Story: As a programmer, I can give my Toyota a year on initialization.
+// The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+// Story: As a programmer, I can make a Volkswagen car.
+// class Volkswagen inherits from class Car
+// create an object called myVolkswagen which is a instance of class Volkswagen
+// Story: As a programmer, I can give my Volkswagen a model on initialization.
+// The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
+// Story: As a programmer, I can give my Volkswagen a year on initialization.
+// The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+// Story: As a programmer, I can give all my cars a lights property. Lights start in the off position.
+// Story: As a programmer, I can turn the lights in all my cars on and off.
+// Story: As a programmer, I can give all my cars a signal property. Turn signal starts in the off position.
+// Story: As a programmer, I can determine the speed of a car. Speed starts at 0 mph.
+//
+class Toyota extends Car {
+  constructor(model, year, wheels){
+    super ()
+    this.model = model
+    this.year = year
+    this.wheels = wheels
+  }
+}
+var myToyota = new Toyota(`corolla`, 2000, 4)
+console.log(myToyota.carProfile());
+class Volkswagen extends Car {
+  constructor(model, year, wheels){
+    super ()
+    this.model = model
+    this.year = year
+    this.wheels = wheels
+  }
+}
+var myVolkswagen = new Volkswagen (`Jetta`, 2016, 4)
+console.log(myVolkswagen.carProfile());
 
 
 
 
+
+
+// Story: As a programmer, I can speed my Tesla up by 10 per acceleration.
+//
+// Story: As a programmer, I can slow my Tesla down by 7 per braking.
