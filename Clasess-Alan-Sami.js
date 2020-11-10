3db8d5d5a1938a744f88bd64d1e6bf973d392d00
mabrouk10 <samimabrouk10@gmail.com>
2020-11-09 16:00:25 -0800

    inheritance practice

--- Clasess-Alan-Sami.js
+++ Clasess-Alan-Sami.js
@@ -85,22 +85,82 @@
 // V = πr2h (r is the radius and h is the height of the cylinder)
 // Write the code that creates three unique cylinder objects
 
-class Cyclinder {
-    constructor(radius, height){
-        this.radius = radius
-        this.height = height
+// class Cyclinder {
+//     constructor(radius, height){
+//         this.radius = radius
+//         this.height = height
+//     }
+//     cylinderProfile(){
+//         return `The cylinder volume is equal to ${(this.radius * this.height * 3.14).toFixed(4)}`
+//     }
+// }
+
+
+// var cylinder1 = new Cyclinder(2,3)
+// var cylinder2 = new Cyclinder(4,5)
+// var cylinder3 = new Cyclinder(6,7)
+
+// console.log(cylinder1.cylinderProfile());
+// console.log(cylinder2.cylinderProfile());
+// console.log(cylinder3.cylinderProfile());
+
+//*********************************************************************************************** */
+//CLASS INHERITANCE
+
+// Story: As a programmer, I can make a car.
+// Write a variable called myCar which is an instance of the class Car
+// Story: As a programmer, I can give my car a model on initialization.
+// The model for the car class can be "generic car"
+// Story: As a programmer, I can give my car a year on initialization.
+// The year for the car class can be "myCar year"
+// Story: As a programmer, I can tell how many wheels myCar has.
+// Calling the method wheels will return 4
+// Story: As a programmer, I can make a Tesla car.
+// class Tesla inherits from class Car
+// Create an object called myTesla which is a instance of class Tesla
+// Story: As a programmer, I can give my Tesla a model on initialization.
+// The model can be inherited from the parent class Car by passing the model 
+//through the constructor() and super() on the child class
+
+
+class Car {
+    constructor(model, year, wheels){
+        this.model = model
+        this.year = year
+        this.wheels = wheels
     }
-    cylinderProfile(){
-        return `The cylinder volume is equal to ${(this.radius * this.height * 3.14).toFixed(4)}`
+    carProfile(){
+        return `This is a ${this.model} car it was made in ${this.year} and it has ${this.wheels} wheels`
+    }
+    carWheels(){
+        return `This car has ${this.wheels}`
+    }
+}
+
+class Tesla extends Car{
+    constructor(model, year, wheels){
+        super()
+        this.model = model
+        this.year = year
+        this.wheels = wheels
+    }
+
+    fuel(){
+        return `${this.model} has clean fuel`
     }
 }
 
 
-var cylinder1 = new Cyclinder(2,3)
-var cylinder2 = new Cyclinder(4,5)
-var cylinder3 = new Cyclinder(6,7)
 
-console.log(cylinder1.cylinderProfile());
-console.log(cylinder2.cylinderProfile());
-console.log(cylinder3.cylinderProfile());
+var myCar = new Car('generic', 2010, 10)
+console.log(myCar.carProfile());
+
+
+var myTesla = new Tesla('model-x', 2020, 4)
+console.log(myTesla.carProfile())
+
+
+
+
+
 
