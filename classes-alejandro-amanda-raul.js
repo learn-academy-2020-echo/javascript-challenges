035943cb34d17bf1f85a53700002fa5d665668c4
REV004 <rsier004@gmail.com>
2020-11-09 16:28:56 -0800

    finish

--- classes-alejandro-amanda-raul.js
+++ classes-alejandro-amanda-raul.js
@@ -92,21 +92,109 @@ class Volume {
   }
 
 calculateVolume() {
-  return this.pi * this.radius * this.height
+  return (this.pi * this.radius * this.height).toFixed(4)
 }
 
 
 }
-
-
-
-
 
 let cylinder1 = new Volume (5, 10)
 console.log(cylinder1.calculateVolume());
+
+let cylinder2 = new Volume (7, 11)
+console.log(cylinder2.calculateVolume());
+
+let cylinder3 = new Volume (8, 12)
+console.log(cylinder3.calculateVolume());
 
 
 
 // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
 //
 // Write the code that creates three unique cylinder objects
+
+
+// Story: As a programmer, I can make a car.
+// Write a variable called myCar which is an instance of the class Car
+
+class Car {
+	constructor(model, year) {
+		this.model = model
+		this.year = year
+
+	}
+wheels() {
+	return 4
+}
+}
+
+let myCar = new Car("toyota", 2020)
+console.log(myCar.wheels())
+
+// Story: As a programmer, I can give my car a model on initialization.
+// The model for the car class can be "generic car"
+// Story: As a programmer, I can give my car a year on initialization.
+// The year for the car class can be "myCar year"
+// Story: As a programmer, I can tell how many wheels myCar has.
+// Calling the method wheels will return 4
+
+class Tesla extends Car {
+	constructor(model, year){
+		super(model, year)
+	}
+}
+
+let myTesla = new Tesla("model x", 2020 )
+console.log(myTesla)
+
+
+
+
+
+
+
+
+// Story: As a programmer, I can make a Tesla car.
+// class Tesla inherits from class Car
+// Create an object called myTesla which is a instance of class Tesla
+// Story: As a programmer, I can give my Tesla a model on initialization.
+// The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
+// Story: As a programmer, I can give my Tesla a year on initialization.
+// The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+// Story: As a programmer, I can make a Toyota car.
+// class Toyota inherits from class Car
+// create an object called myToyota which is a instance of class Toyota
+// Story: As a programmer, I can give my Toyota a model on initialization.
+// The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
+// Story: As a programmer, I can give my Toyota a year on initialization.
+// The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+// Story: As a programmer, I can make a Volkswagen car.
+// class Volkswagen inherits from class Car
+// create an object called myVolkswagen which is a instance of class Volkswagen
+// Story: As a programmer, I can give my Volkswagen a model on initialization.
+// The model can be inherited from the parent class Car by passing the model through the constructor() and super() on the child class
+// Story: As a programmer, I can give my Volkswagen a year on initialization.
+// The year can be inherited from the parent class Car by passing the year through the constructor() and super() on the child class
+// Story: As a programmer, I can give all my cars a lights property. Lights start in the off position.
+//
+// Story: As a programmer, I can turn the lights in all my cars on and off.
+//
+// Story: As a programmer, I can give all my cars a signal property. Turn signal starts in the off position.
+//
+// Story: As a programmer, I can determine the speed of a car. Speed starts at 0 mph.
+//
+// Story: As a programmer, I can speed my Tesla up by 10 per acceleration.
+//
+// Story: As a programmer, I can slow my Tesla down by 7 per braking.
+//
+// Story: As a programmer, I can speed my Toyota up by 5 per acceleration.
+//
+// Story: As a programmer, I can slow my Toyota down by 2 per braking.
+//
+// Story: As a programmer, I can speed my Volkswagen up by 7 per acceleration.
+//
+// Story: As a programmer, I can slow my Volkswagen down by 5 per braking.
+//
+// Story: As a programmer, I can call upon a carInfo method that will tell me all the information about a car.
+//
+// The method can be created in the parent class and accessed by all child classes
