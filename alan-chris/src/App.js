b532c46aae7a8451f57e826ea22012753fa677a4
AlanHausinger <alanh621@gmail.com>
2020-11-12 11:40:24 -0800

    getting started

---
+++ alan-chris/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import Footer from './components/Footer'
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+      <div>
+        <h1> About us </h1>
+
+        <content>My names Alan and I like to play video games, I can't wait to play call of duty
+          cold war today!!! My names Chris and am an avid video game collecter.
+        </content>
+
+       <Footer />
+      </div>
+    )
+  }
+}
+export default App;
