3c78585ad442d50628040d0e1a17e2374816847e
lionsnarl <ryan.sarll@hotmail.com>
2020-11-12 10:52:37 -0800

    initial commit

---
+++ reactApp-p-r/priscila-ryan-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Content from './Components/Content'
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h1> Header Test :) </h1>
+        < Content / >
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ reactApp-p-r/priscila-ryan-app/src/Components/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+  render(){
+    return(
+      <div>
+      <h2>Priscila- Age:25 Loves:Eating Hates:Laundry Enjoys:Watching the Bachelor</h2>
+      <h2>Ryan- Age:25 Loves:Bacon Hates:Dishes Enjoys:Snowboarding</h2>
+      </div>
+    )
+  }
+}
+
+export default Content
