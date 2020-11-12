4c0ea20e8ec2a2ec668941e17dafb9bc68343aa8
mabrouk10 <samimabrouk10@gmail.com>
2020-11-12 10:33:23 -0800

    new commit

---
+++ about-us-s-s/src/Components/Content.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+  render(){
+  return (
+    <div>
+        <h2>Content</h2>
+        
+        <h3>Summer</h3>
+        <ul>
+        <li>Loves Dogs</li>
+        <li>Likes Food</li>
+        <li>Like Surfing</li>
+        </ul>
+
+        <h3>Sami</h3>
+        <ul>
+        <li>Loves Dogs</li>
+        <li>Likes Food</li>
+        <li>Likes Soccer</li>
+        </ul>
+
+    </div>
+   );
+  }
+}
+
+export default Content;
---
+++ about-us-s-s/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render(){
+  return (
+    <div>
+        <h4>By: S + S</h4>
+    </div>
+   );
+  }
+}
+
+export default Footer;
