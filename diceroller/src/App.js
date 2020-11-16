2174b8a8e722eecdc1b1f0397a366caccfbc3a5b
AlanHausinger <alanh621@gmail.com>
2020-11-16 13:30:56 -0800

    rough draft button

---
+++ diceroller/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        possibleRollNums:
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6"
+      }
+  }
+  render() {
+
+    return (
+    <div>
+
+    </div>
+    );
+  }
+}
+export default App;
